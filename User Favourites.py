9d7fcbf7b546291606cbf4ee4c9cb848312ac607
MikeKlostrakis <157991109+MikeKlostrakis@users.noreply.github.com>
2024-02-06 14:17:49 +0000

    Add files via upload

--- User Favourites.py
+++ User Favourites.py
@@ -18,6 +18,23 @@ userhighest = userhighest[['UserID', 'MovieID', 'Rating']]
 
 #df2.to_csv ('UserFavs.csv')
 
+#OPTIONAL: Renaming the MovieIDs to the actual Movie Names
+
+columns2 = ['MovieID', 'Title', 'Gender']
+
+df3 = pd.read_csv("/Users/mike/Desktop/MoviesProject/movies.dat", encoding = "ISO-8859-1", names = columns2, header = None, delimiter="::")
+
+#Remove the Gender column since it is not needed
+
+userhighest.columns = ['UserID', 'Title', 'Rating']
+
+df3 = df3[['MovieID', 'Title']]
+df3.dropna(subset=['MovieID'])
+
+dict = df3.set_index('MovieID')['Title'].to_dict()
+
+userhighest.replace({'Title': dict}, inplace=True)
+
 print(userhighest)
 
 userhighest.to_csv ('UserRanks.csv')
